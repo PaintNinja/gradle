1939fe0b3ba94ea840711b3ec7a690c9c0d03b5b
Vlad Chesnokov <vchesnokov@gradle.com>
2023-11-13 13:44:32 +0400

    Include symlink target to file hash

--- platforms/core-execution/snapshots/src/main/java/org/gradle/internal/vfs/impl/DefaultFileSystemAccess.java
+++ platforms/core-execution/snapshots/src/main/java/org/gradle/internal/vfs/impl/DefaultFileSystemAccess.java
@@ -25,8 +25,6 @@ import org.gradle.internal.file.Stat;
 import org.gradle.internal.file.excludes.FileSystemDefaultExcludesListener;
 import org.gradle.internal.hash.FileHasher;
 import org.gradle.internal.hash.HashCode;
-import org.gradle.internal.hash.Hasher;
-import org.gradle.internal.hash.Hashing;
 import org.gradle.internal.snapshot.FileSystemLocationSnapshot;
 import org.gradle.internal.snapshot.FileSystemSnapshot;
 import org.gradle.internal.snapshot.MissingFileSnapshot;
@@ -41,6 +39,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.locks.Lock;
@@ -110,11 +110,16 @@ public class DefaultFileSystemAccess implements FileSystemAccess, FileSystemDefa
                         return Optional.of(producingSnapshots.guardByKey(location,
                             () -> virtualFileSystem.findSnapshot(location)
                                 .orElseGet(() -> {
-                                    HashCode hashCode = hasher.hash(file, fileMetadata.getLength(), fileMetadata.getLastModified());
-                                    Hasher combinedHasher = Hashing.newHasher(); //TODO: should we support symlinks here?
-                                    combinedHasher.putHash(hashCode);
-                                    combinedHasher.putNull();
-                                    return vfsStorer.store(new RegularFileSnapshot(location, file.getName(), combinedHasher.hash(), fileMetadata));
+                                    String linkTarget = null;
+                                    if (fileMetadata.getAccessType() == FileMetadata.AccessType.VIA_SYMLINK) {
+                                        try {
+                                            linkTarget = Files.readSymbolicLink(file.toPath()).toString();
+                                        } catch (IOException e) {
+                                            // leave as null
+                                        }
+                                    }
+                                    HashCode hashCode = hasher.hash(file, fileMetadata.getLength(), fileMetadata.getLastModified(), linkTarget);
+                                    return vfsStorer.store(new RegularFileSnapshot(location, file.getName(), hashCode, fileMetadata));
                                 })));
                     default:
                         throw new IllegalArgumentException("Unknown file type: " + fileMetadata.getType());
@@ -152,11 +157,8 @@ public class DefaultFileSystemAccess implements FileSystemAccess, FileSystemDefa
             FileMetadata fileMetadata = this.stat.stat(file);
             switch (fileMetadata.getType()) {
                 case RegularFile:
-                    HashCode hash = hasher.hash(file, fileMetadata.getLength(), fileMetadata.getLastModified());
-                    Hasher combinedHasher = Hashing.newHasher(); //TODO: should we support symlinks here?
-                    combinedHasher.putHash(hash);
-                    combinedHasher.putNull();
-                    return vfsStorer.store(new RegularFileSnapshot(location, file.getName(), combinedHasher.hash(), fileMetadata));
+                    HashCode hash = hasher.hash(file, fileMetadata.getLength(), fileMetadata.getLastModified(), null);
+                    return vfsStorer.store(new RegularFileSnapshot(location, file.getName(), hash, fileMetadata));
                 case Missing:
                     return vfsStorer.store(new MissingFileSnapshot(location, fileMetadata.getAccessType()));
                 case Directory:
--- subprojects/core/src/test/groovy/org/gradle/internal/classpath/DefaultCachedClasspathTransformerTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/internal/classpath/DefaultCachedClasspathTransformerTest.groovy
@@ -184,7 +184,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/o_5dd929e3e67781c017a6c4a7cf1c03d4/thing.jar")
+        def cachedFile = testDir.file("cached/o_e161f24809571a55f09d3f820c8e5942/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, None)
@@ -202,7 +202,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/o_5dd929e3e67781c017a6c4a7cf1c03d4/thing.jar")
+        def cachedFile = testDir.file("cached/o_e161f24809571a55f09d3f820c8e5942/thing.jar")
         transformer.transform(classpath, None)
 
         when:
@@ -239,7 +239,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/o_5dd929e3e67781c017a6c4a7cf1c03d4/thing.jar")
+        def cachedFile = testDir.file("cached/o_e161f24809571a55f09d3f820c8e5942/thing.jar")
         transformer.transform(classpath, None)
         modifiedJar(file)
 
@@ -275,7 +275,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/7523467829ce4570a6853afa78c09fab/thing.jar")
+        def cachedFile = testDir.file("cached/9ffa4a434801ef9c7883a0b21244b4d2/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic)
@@ -303,7 +303,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def dir = testDir.file("thing.dir")
         classesDir(dir)
         def classpath = DefaultClassPath.of(dir)
-        def cachedFile = testDir.file("cached/4742a67695fddb260eb0fe3b0220cfc6/thing.dir.jar")
+        def cachedFile = testDir.file("cached/1acfcb20069bc4537c87371818912673/thing.dir.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic)
@@ -333,8 +333,8 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(dir, file)
-        def cachedDir = testDir.file("cached/4742a67695fddb260eb0fe3b0220cfc6/thing.dir.jar")
-        def cachedFile = testDir.file("cached/7523467829ce4570a6853afa78c09fab/thing.jar")
+        def cachedDir = testDir.file("cached/1acfcb20069bc4537c87371818912673/thing.dir.jar")
+        def cachedFile = testDir.file("cached/9ffa4a434801ef9c7883a0b21244b4d2/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic)
@@ -363,7 +363,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file3 = testDir.file("thing3.jar")
         jar(file3)
         def classpath = DefaultClassPath.of(dir, file, dir2, file2, dir3, file3)
-        def cachedFile = testDir.file("cached/o_5dd929e3e67781c017a6c4a7cf1c03d4/thing.jar")
+        def cachedFile = testDir.file("cached/o_e161f24809571a55f09d3f820c8e5942/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, None)
@@ -391,8 +391,8 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file3 = testDir.file("thing3.jar")
         jar(file3)
         def classpath = DefaultClassPath.of(dir, file, dir2, file2, dir3, file3)
-        def cachedDir = testDir.file("cached/4742a67695fddb260eb0fe3b0220cfc6/thing.dir.jar")
-        def cachedFile = testDir.file("cached/7523467829ce4570a6853afa78c09fab/thing.jar")
+        def cachedDir = testDir.file("cached/1acfcb20069bc4537c87371818912673/thing.dir.jar")
+        def cachedFile = testDir.file("cached/9ffa4a434801ef9c7883a0b21244b4d2/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic)
@@ -412,7 +412,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/93abe54b665219ecd150a637754f872d/thing.jar")
+        def cachedFile = testDir.file("cached/73c44f9a393bf3410d6c849d4912d77a/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic, transform)
@@ -446,7 +446,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jarWithStoredResource(file)
         def classpath = DefaultClassPath.of(file)
-        def cachedFile = testDir.file("cached/78340e175a340e41cf8754e92b2f0bdb/thing.jar")
+        def cachedFile = testDir.file("cached/6e068e01b5ecde308301799f9e4d36c5/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform(classpath, BuildLogic)
@@ -463,7 +463,7 @@ class DefaultCachedClasspathTransformerTest extends ConcurrentSpec {
         def file = testDir.file("thing.jar")
         jar(file)
         def remote = new URL("https://somewhere")
-        def cachedFile = testDir.file("cached/o_5dd929e3e67781c017a6c4a7cf1c03d4/thing.jar")
+        def cachedFile = testDir.file("cached/o_e161f24809571a55f09d3f820c8e5942/thing.jar")
 
         when:
         def cachedClasspath = transformer.transform([file.toURI().toURL(), remote], None)
